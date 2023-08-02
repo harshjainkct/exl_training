d14bdb3d71d8ac69e7675238550caf21de494f74
harshjainkct <118609726+harshjainkct@users.noreply.github.com>
2023-08-02 12:27:20 +0530

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class program3
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[5];
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter marks of student " +(i+1) );
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(int c in marks)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
